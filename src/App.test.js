6b931fe1dd210c229e3bc2e6041929e2244df97d
unknown <covarrubias.seb@gmail.com>
2022-07-01 21:14:24 -0700

    Created test descriptions for user stories

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,61 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('User can Login with Google using Firebase Auth', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
+
+test('User can Logout with Google using Firebase Auth', () => {
+  render(<App />);
+});
+
+test('User can visit a unique server (GET /servers/:id/channel/0)', () => {
+  render(<App />);
+});
+
+test('User can visit different channels in a server (GET /servers/:id/channel/:index)', () => {
+  render(<App />);
+});
+
+test('User can post messages in a server channel (PUT /servers/:id/channel/:index/messages/:id)', () => {
+  render(<App />);
+});
+
+test('User can edit their own messages (POST /servers/:id/channel/:index/messages/:id)', () => {
+  render(<App />);
+});
+
+test('User can delete their own messages (DELETE /servers/:id/channel/:index/messages/:id)', () => {
+  render(<App />);
+});
+
+test('User can create new servers (PUT /servers/:id)', () => {
+  render(<App />);
+});
+
+test('User can edit server names of servers they made (POST /servers/:id)', () => {
+  render(<App />);
+});
+
+test('User can delete servers they made (DELETE /servers/:id)', () => {
+  render(<App />);
+});
+
+test('User can create new channels in servers they made (PUT /servers/:id/channel/:index)', () => {
+  render(<App />);
+});
+
+test('User can edit channel names in servers they made (POST /servers/:id/channel/:index)', () => {
+  render(<App />);
+});
+
+test('User can delete channels in servers they made (DELETE /servers/:id/channel/:index)', () => {
+  render(<App />);
+});
+
+test('User can delete messages in servers they made (DELETE /servers/:id/channel/:index/messages/:id)', () => {
+  render(<App />);
+});
+
+
+
